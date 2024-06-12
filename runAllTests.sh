38ddb02764610d309c1635e648ad347aa2062265
Aleksander Haouem <4leeeksha@gmail.com>
2024-06-12 09:04:04 +0200

    fixed: fixed runAllTests so its clean

--- runAllTests.sh
+++ runAllTests.sh
@@ -1,28 +1,15 @@
 #!/bin/bash
 
-## Excersie 1
-echo "Running tests for ex1..."
-cd src/ex1 || exit
-mkdir -p out
-javac -d out TextAnalysis.java
-javac -d out -cp out:../../junit-platform-console-standalone-1.3.1.jar TextAnalysisTest.java
-java -jar ../../junit-platform-console-standalone-1.3.1.jar --class-path out --scan-class-path
-cd ../..
+cd src || exit
+exercises=($(ls -d ex*))
+cd ..
 
-## Excersie 2
-echo "Running tests for ex2..."
-cd src/ex2 || exit
-mkdir -p out
-javac -d out User.java UserAnalyzer.java
-javac -d out -cp out:../../junit-platform-console-standalone-1.3.1.jar UserAnalyzerTest.java
-java -jar ../../junit-platform-console-standalone-1.3.1.jar --class-path out --scan-class-path
-cd ../..
-
-## Excersie 3
-echo "Running tests for ex3..."
-cd src/ex3 || exit
-mkdir -p out
-javac -d out Product.java ProductRepository.java
-javac -d out -cp out:../../junit-platform-console-standalone-1.3.1.jar ProductRepositoryTest.java
-java -jar ../../junit-platform-console-standalone-1.3.1.jar --class-path out --scan-class-path
-cd ../..
+for exercise in ${exercises[@]}; do
+    echo "Running tests for $exercise..."
+    cd src/$exercise || exit
+    mkdir -p out
+    javac -d out $(ls | grep -v Test.java$ | grep .java$)
+    javac -d out -cp out:../../junit-platform-console-standalone-1.3.1.jar *Test.java
+    java -jar ../../junit-platform-console-standalone-1.3.1.jar --class-path out --scan-class-path
+    cd ../..
+done
